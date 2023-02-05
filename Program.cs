3ea7736154599818c25b21b5d1bfdcb5f7e4a9bc
Nadya <nadya0707@yandex.ru>
2023-02-05 17:12:22 +0300

    commit 1

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Введите числа через пробел: ");
+int [] array = Console.ReadLine().Split(" ").Select(x=>int.Parse(x)).ToArray();
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+        {
+            count++;
+        }
+}
+Console.WriteLine($"{count}");
